6ffbe42e7fbd2b67c47d0526c757711b879ad616
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-12-21 18:38:11 +0200

    add HealthController and update dependencies for web support

---
+++ src/main/kotlin/org/spendoo/spendoo/HealthController.kt
@@ -0,0 +1,11 @@
+package org.spendoo.spendoo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+    @GetMapping("/")
+    fun home(): String = "OK"
+}
+
