032a0dbbf0d67030b0cbb151a62fb2b0968d18d3
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2026-03-13 20:06:56 +0200

    feat: enhance category and budget repositories with new query methods

--- categories/src/main/kotlin/org/spendoo/categories/repository/CategoryRepository.kt
+++ categories/src/main/kotlin/org/spendoo/categories/repository/CategoryRepository.kt
@@ -1,13 +1,66 @@
 package org.spendoo.categories.repository
 
 import org.spendoo.categories.entity.Category
+import org.spendoo.categories.service.model.CategoryParams
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
+import org.springframework.data.jpa.repository.Query
 import java.util.*
 
-@Repository
-interface CategoryRepository : JpaRepository<Category, UUID> {
-    fun findAllByUserIdOrUserIdIsNullAndIsDeletedFalse(userId: UUID, pageable: Pageable): Page<Category>
+interface CategoryRepository : JpaRepository<Category, UUID>
+{
+    @Query(
+        """
+            SELECT NEW org.spendoo.categories.service.model.CategoryParams(
+                c.id,
+                c.categoryName,
+                c.categoryIcon,
+                c.priority,
+                c.leftOverOptions,
+                b.id,
+                b.amount,
+                b.carryOver,
+                b.period,
+                b.startDate,
+                b.endDate
+            )
+            FROM Category c
+            LEFT JOIN Budget b
+                ON c.id = b.category.id
+            WHERE c.userId = :userId
+                AND b.isActive = true
+                AND c.isDeleted = false
+        """
+    )
+    fun getExistCategoriesWithBudget(userId: UUID, pageable: Pageable): Page<CategoryParams>
+
+    @Query(
+        """
+            SELECT NEW org.spendoo.categories.service.model.CategoryParams(
+                c.id,
+                c.categoryName,
+                c.categoryIcon,
+                c.priority,
+                c.leftOverOptions,
+                b.id,
+                b.amount,
+                b.carryOver,
+                b.period,
+                b.startDate,
+                b.endDate
+            )
+            FROM Category c
+            LEFT JOIN Budget b
+                ON c.id = b.category.id
+            WHERE
+                c.id = :categoryId
+                AND c.userId = :userId
+                AND b.isActive = true
+                AND c.isDeleted = false
+        """
+    )
+    fun getExistCategoryWithBudget(categoryId: UUID, userId: UUID): CategoryParams?
+
+    fun findByIdAndUserIdAndIsDeletedFalse(id: UUID, userId: UUID): Category?
 }
