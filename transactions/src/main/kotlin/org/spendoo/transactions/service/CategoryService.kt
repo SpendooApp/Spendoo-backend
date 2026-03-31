fd8f4c0e324d72faf3c5072ece6934a1b059a3d4
Israa Mohamed <saramohamed2315@gmail.com>
2026-03-31 14:02:13 +0200

    feat(transaction): Import BigDecimal in CategoryService

--- transactions/src/main/kotlin/org/spendoo/transactions/service/CategoryService.kt
+++ transactions/src/main/kotlin/org/spendoo/transactions/service/CategoryService.kt
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
+import java.math.BigDecimal
 
 @Service
 class CategoryService(
