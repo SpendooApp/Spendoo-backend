1e3d781fc94aa1f81973a1265d447c98fabc670c
IsraaMohamed <saramohamed2315@gmail.com>
2026-06-14 20:50:58 +0300

    refactor(notifications): migrate email listener to use ApiClient
    - Replaced legacy RestTemplate with the new ApiClient.
    - Removed unused RestTemplateConfig and SendEmailRequest DTO.
    - Added http-client dependency to the notifications module.

--- notifications/src/main/kotlin/org/spendoo/notifications/AsyncEmailListener.kt
+++ notifications/src/main/kotlin/org/spendoo/notifications/AsyncEmailListener.kt
@@ -1,15 +1,16 @@
 package org.spendoo.notifications
 
+import org.spendoo.client.ApiClient
 import org.slf4j.LoggerFactory
 import org.spendoo.events.notifications.EmailEvent
 import org.springframework.context.event.EventListener
+import org.springframework.http.HttpMethod
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
-import org.springframework.web.client.RestTemplate
 
 @Component
 class AsyncEmailListener(
-    private val restTemplate: RestTemplate,
+    private val apiClient: ApiClient,
 ) {
     private val log = LoggerFactory.getLogger(javaClass)
 
@@ -19,14 +20,19 @@ class AsyncEmailListener(
     @EventListener
     fun handleEmailEvent(event: EmailEvent) {
         try {
-            val request = SendEmailRequest(
-                email = event.to,
-                subject = event.subject,
-                message = event.text
+            val requestBody = mapOf(
+                "email" to event.to,
+                "subject" to event.subject,
+                "message" to event.text
             )
-            val response = restTemplate.postForEntity(emailServiceUrl, request, String::class.java)
+            apiClient.call(String::class.java){
+                path = emailServiceUrl
+                method = HttpMethod.POST
+                body = requestBody
+                addToken = false
+            }
 
-            log.info("Email sent to ${event.to} with subject '${event.subject}'. Response: ${response.statusCode}")
+            log.info("Email sent successfully to ${event.to} with subject '${event.subject}'.")
         }
         catch (e: Exception) {
             log.error("Failed to send email to ${event.to}", e)
--- notifications/src/main/kotlin/org/spendoo/notifications/SendEmailRequest.kt
+++
@@ -1,7 +0,0 @@
-package org.spendoo.notifications
-
-data class SendEmailRequest(
-    val email: String,
-    val subject: String,
-    val message: String
-)
