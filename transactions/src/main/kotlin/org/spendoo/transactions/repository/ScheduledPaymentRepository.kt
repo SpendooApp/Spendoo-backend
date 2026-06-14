d5c55f2779f15e3126d62d48c24271d59d6990cc
IsraaMohamed <saramohamed2315@gmail.com>
2026-06-14 21:03:22 +0300

    chore: rename repository
    - Renamed ScheduledPaymentsRepository to singular ScheduledPaymentRepository for naming consistency.

--- transactions/src/main/kotlin/org/spendoo/transactions/repository/ScheduledPaymentRepository.kt
+++ transactions/src/main/kotlin/org/spendoo/transactions/repository/ScheduledPaymentRepository.kt
@@ -6,12 +6,10 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
-import org.springframework.stereotype.Repository
 import java.math.BigDecimal
 import java.time.LocalDateTime
 import java.util.UUID
 
-@Repository
 interface ScheduledPaymentRepository : JpaRepository<ScheduledPayment, UUID> {
     fun findAllByUserId(userId: UUID, pageable: Pageable): Page<ScheduledPayment>
 
