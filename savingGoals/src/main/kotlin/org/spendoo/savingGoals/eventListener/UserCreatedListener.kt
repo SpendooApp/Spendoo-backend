c7f92932c346dbacea61552acd92bd6ba716c0fd
Amany Mohamed <amanyhussein133@gmail.com>
2026-06-23 23:08:50 +0300

    feat(listener): rename UserCreatedListener component for clarity

--- savingGoals/src/main/kotlin/org/spendoo/savingGoals/eventListener/UserCreatedListener.kt
+++ savingGoals/src/main/kotlin/org/spendoo/savingGoals/eventListener/UserCreatedListener.kt
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
-@Component
+@Component("AchievementUserCreatedListener")
 
 class UserCreatedListener(
     private val achievementService: AchievementService
