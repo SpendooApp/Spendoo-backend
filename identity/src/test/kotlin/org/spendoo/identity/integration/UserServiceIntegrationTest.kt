02753bda5763f17111f88c3263049cd1b5c2ca44
IsraaMohamed <saramohamed2315@gmail.com>
2026-06-25 18:10:55 +0300

    test: fix DataIntegrityViolationException in UserService setup

--- identity/src/test/kotlin/org/spendoo/identity/integration/UserServiceIntegrationTest.kt
+++ identity/src/test/kotlin/org/spendoo/identity/integration/UserServiceIntegrationTest.kt
@@ -13,6 +13,10 @@ import org.spendoo.identity.api.dto.request.UpdateProfileRequest
 import org.spendoo.identity.entity.Gender
 import org.spendoo.identity.entity.User
 import org.spendoo.identity.exception.UserNotFoundException
+import org.spendoo.identity.repository.EmailVerificationRepository
+import org.spendoo.identity.repository.FollowCodeRepository
+import org.spendoo.identity.repository.FollowRepository
+import org.spendoo.identity.repository.RefreshTokenRepository
 import org.spendoo.identity.repository.UserRepository
 import org.spendoo.identity.service.UserService
 import org.spendoo.storage.service.ImageStorageService
@@ -35,6 +39,18 @@ class UserServiceIntegrationTest {
     private lateinit var userRepository: UserRepository
 
     @Autowired
+    private lateinit var followRepository: FollowRepository
+
+    @Autowired
+    private lateinit var followCodeRepository: FollowCodeRepository
+
+    @Autowired
+    private lateinit var refreshTokenRepository: RefreshTokenRepository
+
+    @Autowired
+    private lateinit var emailVerificationRepository: EmailVerificationRepository
+
+    @Autowired
     private lateinit var imageStorageService: ImageStorageService
 
     @Autowired
@@ -42,6 +58,10 @@ class UserServiceIntegrationTest {
 
     @BeforeEach
     fun setUp() {
+        followRepository.deleteAll()
+        followCodeRepository.deleteAll()
+        refreshTokenRepository.deleteAll()
+        emailVerificationRepository.deleteAll()
         userRepository.deleteAll()
         clearMocks(imageStorageService, answers = false, recordedCalls = true)
     }
