4cadc4b8cc039109b9a6f50ec5e120dd1bd7e069
Amany Mohamed <amanyhussein133@gmail.com>
2026-07-03 15:23:54 +0300

    refactor(subscription): remove unused ComponentScan import from ChatbotTestApplication

--- chatbot/src/test/kotlin/ChatbotTestApplication.kt
+++ chatbot/src/test/kotlin/ChatbotTestApplication.kt
@@ -10,7 +10,6 @@ import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.persistence.autoconfigure.EntityScan
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootConfiguration
