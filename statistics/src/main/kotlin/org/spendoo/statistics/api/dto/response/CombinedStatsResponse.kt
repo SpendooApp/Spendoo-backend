8b9ec7ffd8cbd0491991e2e892aeecd1b749f1cf
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2026-07-05 20:38:14 +0300

    Update CombinedStatsResponse.kt

--- statistics/src/main/kotlin/org/spendoo/statistics/api/dto/response/CombinedStatsResponse.kt
+++ statistics/src/main/kotlin/org/spendoo/statistics/api/dto/response/CombinedStatsResponse.kt
@@ -3,7 +3,7 @@ package org.spendoo.statistics.api.dto.response
 import com.fasterxml.jackson.annotation.JsonAlias
 
 data class CombinedStatsResponse(
-    @field:JsonAlias("financial_stats")
+    @field:JsonAlias("financial_stats_forecast")
     val financialStats: FinancialStatsResponse,
     @field:JsonAlias("budget_status")
     val budgetStatus: BudgetStatusResponse,
